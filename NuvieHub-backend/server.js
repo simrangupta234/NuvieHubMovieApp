33e070acfc655c192c8e84d0cc844d4c0d317b15
simrangupta234 <simrangupta172002@gmail.com>
2024-08-13 22:33:58 +0530

    deployed fronted added

--- NuvieHub-backend/server.js
+++ NuvieHub-backend/server.js
@@ -22,7 +22,11 @@ const upload = multer({ storage: storage });
 connectDb();
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: [
+      "http://127.0.0.1:5173",
+      "http://localhost:5173",
+      "https://nuviehub2.onrender.com/",
+    ],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 204,
