d683f55cc9eab3ee86bc6d76e42847669888beaa
Simran Gupta <72262854+simrangupta234@users.noreply.github.com>
2024-08-13 21:54:49 +0530

    Update server.js

--- NuvieHub-backend/server.js
+++ NuvieHub-backend/server.js
@@ -73,6 +73,9 @@ app.use("/profiles", express.static("profiles"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to NuvieHub Movie App API");
+});
 app.use("/api/movies", require("./routes/movieRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
